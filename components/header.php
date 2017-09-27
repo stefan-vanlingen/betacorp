01c9245b289cee481ab5380785d560454fd76992
stefan-vanlingen <s.vanlingen5@gmail.com>
2017-09-27 21:50:41 +0200

    added basic structure for now

---
+++ components/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+	<div class="navbar is-transparent">
+
+		<div class="navbar-logo"></div>	
+
+		<nav class="nav">
+			<a href="#" class="nav-item is-active">Home</a>
+			<a href="#" class="nav-item">Over ons</a>
+			<a href="#" class="nav-item">Portfolio</a>
+			<a href="#" class="nav-item">Contact</a>
+		</nav>
+
+	</div>
+</header
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Beta Corp</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<?php include "components/header.php" ?>
+
+	<main class="main">
+		<section class="section">
+			
+		</section>
+	</main>
+
+<?php include "components/footer.php" ?>
+	
+</body>
+</html>
