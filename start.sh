e6302a597efed9adf76315d9e8ccfb573b40d1ea
Bart ten Brinke <info@retrosync.com>
2017-10-18 12:17:14 +0200

    Add -d

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 MYPWD=${PWD}
 sass ./css/main.scss ./css/main.css
 open http://localhost:80 &
-docker run -t -i -p 80:80 -v $MYPWD:/var/www "eriksencosta/php-dev:latest"
+docker run -d -p 80:80 -v $MYPWD:/var/www "eriksencosta/php-dev:latest"
 sass --watch ./css/main.scss:./css/main.css
 
