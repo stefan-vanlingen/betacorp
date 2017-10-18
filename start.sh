538bb28cb3172d578e1fb2b8a745e2da900619ae
Bart ten Brinke <info@retrosync.com>
2017-10-18 12:14:01 +0200

    Ad development toolign

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MYPWD=${PWD}
+sass ./css/main.scss ./css/main.css
+open http://localhost:80 &
+docker run -t -i -p 80:80 -v $MYPWD:/var/www "eriksencosta/php-dev:latest"
+sass --watch ./css/main.scss:./css/main.css
+
